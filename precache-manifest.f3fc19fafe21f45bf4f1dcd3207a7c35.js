7cfb56163c4226fa2097f7f0ffcbf00903088685
gdadulla <gdadulla@albany.edu>
2021-01-23 01:49:40 -0500

    Updates

---
+++ precache-manifest.f3fc19fafe21f45bf4f1dcd3207a7c35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fa1101c66bbc8dff91b229a9e6507c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9c52c424233798db2b",
+    "url": "/static/css/2.54f98571.chunk.css"
+  },
+  {
+    "revision": "d1a81c173d21364ce8f8",
+    "url": "/static/css/main.6ecb5a6a.chunk.css"
+  },
+  {
+    "revision": "ca9c52c424233798db2b",
+    "url": "/static/js/2.5e3cf89f.chunk.js"
+  },
+  {
+    "revision": "5619b624ac1c824e51b00fe089889ae6",
+    "url": "/static/js/2.5e3cf89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a81c173d21364ce8f8",
+    "url": "/static/js/main.6c680935.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
